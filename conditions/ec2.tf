ba715a444c99be9874a1e49afd7cdd936ee2e25d
DevPhani23 <devphani23600@gmail.com>
2024-05-30 12:07:09 +0530

    terraform conditions

---
+++ conditions/ec2.tf
@@ -0,0 +1,13 @@
+resource "aws_instance" "db" {
+    ami = var.image_id
+    instance_type = var.instance_type == "db" ? "t3.small" : "t3.micro"
+    
+    tags = {
+      Name = "DevPhani"
+    }
+
+}
+
+
+
+  
